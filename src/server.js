38a75ba50862c053ab6216541b0dfbbb764ccd9e
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-06-06 10:23:54 +0530

    removed comments

--- src/server.js
+++ src/server.js
@@ -16,21 +16,15 @@ connectDB(); // connect to the database
 app.use(cors());
 app.use(express.json()); //parse JSON bodies
 
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send(`Server is running`);
-// });
+app.get("/", (req, res) => {
+  res.send(`Server is running`);
+});
 
 app.get("/twiter", (req, res) => {
   res.send(`Checking`);
 });
-
-app.use((req, res, next) => {
-  console.log("Fallback middleware hit");
-  res.status(404).json({ message: "Route not found" });
-});
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
