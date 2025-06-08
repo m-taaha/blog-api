dbe97c1e4f078367710deaf97fd93571475d4bd2
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-06-08 23:58:21 +0530

    Add authentication routes for register, login, and user profile

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+const isAuthenticated = async (req, res, next) => {
+  const token = req.body.cookies; // token from browser
+
+  if (!token) {
+    return res.status(400).json({ message: `Unauthorized user` });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select(-password);
+
+    if (!user) {
+      return res.status(401).json({ message: `Unauthorized, User not found` });
+    }
+
+    req.user = user; //aTTach user req to user
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: `Invalid or token expired` });
+  }
+};
+
+export { isAuthenticated };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,20 @@
 import express from "express";
 import { registerUser } from "../controllers/register.controller.js";
 import { loginUser } from "../controllers/login.controller.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+//public routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+//protected routes
+router.get("/me", isAuthenticated, (req, res) => {
+  res.status(200).json({
+    message: "User data fetched Successfully",
+    user: req.user,
+  });
+});
 
 export default router;
